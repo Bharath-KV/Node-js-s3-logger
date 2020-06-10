d38393bb1e0118e501342dce74e12d0b2248f4ad
Bharath-KV <42246351+Bharath-KV@users.noreply.github.com>
2020-06-11 00:24:31 +0530

    Logger functionality using winston

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const winston = require('winston')
+
+const logger = winston.createLogger({
+    transports: [ new winston.transports.Console() ]
+})
+
+logger.on('data', (chunk => { // log listener
+    console.log(JSON.stringify(chunk))
+}))
 
 const app = express()
 
@@ -7,6 +16,7 @@ app.get('/some-endpoint', (req, res) => {
         // Your logic here
         return res.status(200).json({ /* success response */ })
     } catch (error) {
+        logger.error(error.toString()) // log emitter
         return res.status(500).json({ /* error response */ })
     }
 })
