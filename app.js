94079134f148f13f7df548aef5cd045ba9b6d836
Bharath-KV <42246351+Bharath-KV@users.noreply.github.com>
2020-06-11 00:28:40 +0530

    call s3 uploader and pass stringyfied json log

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const winston = require('winston')
+const s3_upload = require('./s3_upload')
 
 const logger = winston.createLogger({
     transports: [ new winston.transports.Console() ]
 })
 
 logger.on('data', (chunk => { // log listener
-    console.log(JSON.stringify(chunk))
+    s3_upload(JSON.stringify(chunk)) // call s3 uploader and pass stringyfied json log
 }))
 
 const app = express()
